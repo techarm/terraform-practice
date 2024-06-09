34c43de3fe52a9741ca1004846e6e09ab82e2fa0
techarm <techarmdev@gmail.com>
2024-06-09 11:24:09 +0900

    Backendsの環境ごと設定

--- 04-backends/providers.tf
+++ 04-backends/providers.tf
@@ -11,12 +11,7 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "terraform-course-remote-state-backend"
-    key    = "04-backends/state.tfstate"
-    region = "ap-northeast-3"
-    # dynamodb_table = "my-dynamodb-state" # lock state
-  }
+  backend "s3" {}
 }
 
 provider "aws" {
