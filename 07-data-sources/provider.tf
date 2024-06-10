da858fdb2ef9f190e486eb4e0af0fd9f0b147461
techarm <techarmdev@gmail.com>
2024-06-10 22:55:33 +0900

    Data Sources

---
+++ 07-data-sources/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "~> 1.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-3"
+}
+
+provider "aws" {
+  region = "ap-northeast-3"
+  alias  = "osaka"
+}
