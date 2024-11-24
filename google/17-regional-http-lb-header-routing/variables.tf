c53c4c28c19cdab2a7d086e6b2bb42b2cc2e2a8c
techarm <techarmdev@gmail.com>
2024-11-24 16:42:32 +0900

    GCP Load Balancer with Header based Routing

---
+++ google/17-regional-http-lb-header-routing/variables.tf
@@ -0,0 +1,29 @@
+variable "gcp_project" {
+  description = "Project in which GPC resources to be created"
+  type        = string
+  default     = "gcplearn202411"
+}
+
+variable "gcp_region" {
+  description = "Region which GPC resources to be created"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "machine_type" {
+  description = "Compute Engine Machine Type"
+  type        = string
+  default     = "e2-small"
+}
+
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type        = string
+  default     = "dev"
+}
+
+variable "business_divsion" {
+  description = "Business Division in the large organization this Infrastructure belongs"
+  type        = string
+  default     = "sap"
+}
