c482813d2ff7531e6a050a876f5e8c6b1e38c5d7
techarm <techarmdev@gmail.com>
2024-11-24 16:35:35 +0900

    GCP Load Balancer with Domain Name based Routing

---
+++ google/16-regional-http-lb-host-routing/app2-mig-healthcheck.tf
@@ -0,0 +1,11 @@
+resource "google_compute_region_health_check" "myapp2" {
+  name                = "${local.name}-myapp2"
+  check_interval_sec  = 5
+  timeout_sec         = 5
+  healthy_threshold   = 2
+  unhealthy_threshold = 3
+  http_health_check {
+    request_path = "/index.html"
+    port         = "80"
+  }
+}
