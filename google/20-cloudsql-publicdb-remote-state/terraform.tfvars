9c06c02ca361bca717aecac1f76c828d4c9f1cba
techarm <techarmdev@gmail.com>
2024-11-25 20:44:11 +0900

    GCP Cloud SQL Public IP + Terraform Remote Backend as Cloud Storage

---
+++ google/20-cloudsql-publicdb-remote-state/terraform.tfvars
@@ -0,0 +1,5 @@
+gcp_project               = "gcplearn202411"
+gcp_region                = "us-central1"
+environment               = "dev"
+business_divsion          = "hr"
+cloudsql_database_version = "MYSQL_8_0"
---
+++ google/20-cloudsql-publicdb-remote-state/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.9.8"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.9.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "gcplearn-tfstate"
+    prefix = "cloudsql/publicdb"
+  }
+}
+
+provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region
+}
